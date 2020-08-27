2354030ac2af5f6cacf8c45212ae0f78124dc02d
helenxuyang <helen.xu.yang@gmail.com>
2020-08-26 21:25:20 -0400

    New card UI

--- lib/Home.dart
+++ lib/Home.dart
@@ -58,56 +58,78 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextStyle logisticsStyle = TextStyle(
+        fontSize: 14
+    );
+    TextStyle titleStyle = TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold
+    );
+    TextStyle subtitleStyle = TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.bold
+    );
+    TextStyle secondaryStyle = TextStyle(
+      fontSize: 14,
+      fontStyle: FontStyle.italic,
+      color: Colors.grey
+    );
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Card(
           child: Padding(
-            padding: EdgeInsets.all(16),
+            padding: EdgeInsets.all(24),
             child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  Text(title, style: titleStyle),
                   SizedBox(height: 8),
-                  Tags(tags),
-                  SizedBox(height: 8),
-                  Text('Organized by $organizer'),
-                  SizedBox(height: 8),
-                  Text(description),
-                  Padding(
-                    padding: EdgeInsets.only(top: 8, bottom: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  Row(
                       children: [
+                        Icon(Icons.calendar_today),
+                        SizedBox(width: 4),
+                        Text(DateFormat('E').add_MMMd().format(dateTime), style: logisticsStyle),
+                        SizedBox(width: 16),
                         Icon(Icons.access_time),
-                        SizedBox(width: 8),
-                        Text(DateFormat('Md').add_jm().format(dateTime)),
+                        SizedBox(width: 4),
+                        Text(DateFormat('jm').format(dateTime), style: logisticsStyle),
                         SizedBox(width: 16),
                         Icon(Icons.person),
-                        SizedBox(width: 8),
-                        Text(currentAttendees.toString() + '/' + maxAttendees.toString()),
-                      ],
-                    ),
+                        SizedBox(width: 4),
+                        Text(currentAttendees.toString() + '/' + maxAttendees.toString(), style: logisticsStyle),
+                      ]
+                  ),
+                  SizedBox(height: 8),
+                  Text('Organized by: $organizer', style: subtitleStyle),
+                  SizedBox(height: 8),
+                  Text(description),
+                  SizedBox(height: 8),
+                  Text(tags.map((tag) => '#' + tag).join('  '), style: secondaryStyle),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8, bottom: 8),
                   ),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      OutlineButton(
-                          borderSide: BorderSide(color: Colors.blue),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-                          child: Text('SIGN UP', style: TextStyle(color: Colors.blue)),
-                          onPressed: () {
-                            //TODO: add sign up
-                          }
-                      ),
-                      SizedBox(width: 16),
-                      OutlineButton(
-                          borderSide: BorderSide(color: Colors.blue),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-                          child: Text('SHARE', style: TextStyle(color: Colors.blue)),
-                          onPressed: () {
-                            //TODO: add share
-                          }
-                      )
-                    ]
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        OutlineButton(
+                            borderSide: BorderSide(color: Colors.blue),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                            child: Text('SIGN UP', style: TextStyle(color: Colors.blue)),
+                            onPressed: () {
+                              //TODO: add sign up
+                            }
+                        ),
+                        SizedBox(width: 16),
+                        OutlineButton(
+                            borderSide: BorderSide(color: Colors.blue),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                            child: Text('SHARE', style: TextStyle(color: Colors.blue)),
+                            onPressed: () {
+                              //TODO: add share
+                            }
+                        )
+                      ]
                   )
                 ]
             ),
