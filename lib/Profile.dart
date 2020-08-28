fb00c377d0e17f07d39a59df6344e36138a4ffd3
Saleh Hassen <saleh99er@gmail.com>
2020-08-28 09:16:29 -0400

    google meet asset

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -11,31 +11,34 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String userID = Provider.of<CurrentUserInfo>(context).id;
-    TextStyle subtitleStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+    TextStyle subtitleStyle =
+        TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
     return Padding(
       padding: const EdgeInsets.all(24),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           if (id != userID)
-          Container(
-            width: 50,
-            child: FlatButton(
-                splashColor: Colors.white,
-                highlightColor: Colors.white,
-                padding: EdgeInsets.only(left: 0, right: 4),
-                child: Align(
-                    child: Text('Back',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.normal)),
-                    alignment: Alignment.centerLeft),
-                onPressed: () {
-                  Navigator.pop(context);
-                }
+            Container(
+              width: 50,
+              child: FlatButton(
+                  splashColor: Colors.white,
+                  highlightColor: Colors.white,
+                  padding: EdgeInsets.only(left: 0, right: 4),
+                  child: Align(
+                      child: Text('Back',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.normal)),
+                      alignment: Alignment.centerLeft),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
             ),
-          ),
           StreamBuilder(
-            stream: FirebaseFirestore.instance.collection('users').doc(id).snapshots(),
+            stream: FirebaseFirestore.instance
+                .collection('users')
+                .doc(id)
+                .snapshots(),
             builder: (context, snapshot) {
               if (!snapshot.hasData) {
                 return CircularProgressIndicator();
@@ -44,55 +47,63 @@ class ProfilePage extends StatelessWidget {
               String userID = Provider.of<CurrentUserInfo>(context).id;
               return Expanded(
                 child: Scrollbar(
-                  child: ListView(
-                      children: [
-                        Text(doc.get('firstName') + ' ' + doc.get('lastName'), style: Theme.of(context).textTheme.headline1),
-                        Text(doc.get('major') + ' ' + doc.get('classYear').toString(), style: TextStyle(fontSize: 18)),
-                        Text(doc.get('status'), style: TextStyle(fontSize: 18, color: Color.fromRGBO(0xeb, 0x8a, 0x90, 1.0))),
-                        SizedBox(height: 16),
-                        Text('Bio', style: subtitleStyle),
-                        Text(doc.get('bio'), style: TextStyle(fontSize: 16)),
-                        SizedBox(height: 16),
-                        Text('Activities', style: subtitleStyle),
-                        Wrap(
-                          children: List<String>.from(doc.get('interestedTags')).map((str) => InputChip(label: Text(str))).toList(),
-                          spacing: 4,
-                          runSpacing: -8,
-                        ),
-                        SizedBox(height: 16),
-                        Text('Classes', style: subtitleStyle),
-                        Wrap(
-                          children: List<String>.from(doc.get('classes')).map((str) => InputChip(label: Text(str))).toList(),
-                          spacing: 4,
-                          runSpacing: -8,
-                        ),
-                        SizedBox(height: 16),
-                        Text('Upcoming Events', style: subtitleStyle),
-                        StreamBuilder(
-                            stream: FirebaseFirestore.instance
-                                .collection('events')
-                                .where('attendees', arrayContains: userID)
-                                .where('dateTime', isGreaterThan: Timestamp.now()).snapshots(),
-                            builder: (context, snapshot) {
-                              if (!snapshot.hasData) {
-                                return Container();
-                              }
-                              List<DocumentSnapshot> docs = snapshot.data.docs;
-                              if (docs.isEmpty) {
-                                return Text('No events yet!');
-                              }
-                              return SingleChildScrollView(
-                                  scrollDirection: Axis.horizontal,
-                                  child: Row(
-                                      children: List<EventCard>.from(snapshot.data.documents.map((doc) {
-                                        return EventCard(Event.fromDoc(doc));
-                                      }))
-                                  )
-                              );
-                            }
-                        )
-                      ]
-                  ),
+                  child: ListView(children: [
+                    Text(doc.get('firstName') + ' ' + doc.get('lastName'),
+                        style: Theme.of(context).textTheme.headline1),
+                    Text(
+                        doc.get('major') +
+                            ' ' +
+                            doc.get('classYear').toString(),
+                        style: TextStyle(fontSize: 18)),
+                    Text(doc.get('status'),
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Color.fromRGBO(0xeb, 0x8a, 0x90, 1.0))),
+                    SizedBox(height: 16),
+                    Text('Bio', style: subtitleStyle),
+                    Text(doc.get('bio'), style: TextStyle(fontSize: 16)),
+                    SizedBox(height: 16),
+                    Text('Activities', style: subtitleStyle),
+                    Wrap(
+                      children: List<String>.from(doc.get('interestedTags'))
+                          .map((str) => InputChip(label: Text(str)))
+                          .toList(),
+                      spacing: 4,
+                      runSpacing: -8,
+                    ),
+                    SizedBox(height: 16),
+                    Text('Classes', style: subtitleStyle),
+                    Wrap(
+                      children: List<String>.from(doc.get('classes'))
+                          .map((str) => InputChip(label: Text(str)))
+                          .toList(),
+                      spacing: 4,
+                      runSpacing: -8,
+                    ),
+                    SizedBox(height: 16),
+                    Text('Upcoming Events', style: subtitleStyle),
+                    StreamBuilder(
+                        stream: FirebaseFirestore.instance
+                            .collection('events')
+                            .where('attendees', arrayContains: userID)
+                            .where('dateTime', isGreaterThan: Timestamp.now())
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return Container();
+                          }
+                          List<DocumentSnapshot> docs = snapshot.data.docs;
+                          if (docs.isEmpty) {
+                            return Text('No events yet!');
+                          }
+                          return SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(children: List<EventCard>.from(
+                                  snapshot.data.documents.map((doc) {
+                                return EventCard(Event.fromDoc(doc));
+                              }))));
+                        })
+                  ]),
                 ),
               );
             },
